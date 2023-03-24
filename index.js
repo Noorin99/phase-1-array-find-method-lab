a1b1c14314c9f1e8b0019395861b5f30c12b5b09
Nur <noorin.sa.99@gmail.com>
2023-03-24 14:45:36 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const superbowlWin = (record) => {
+  const result = record.find((obj) => obj.result === "W");
+  if (result) {
+    return result.year;
+  } else {
+    return undefined;
+  }
+};
